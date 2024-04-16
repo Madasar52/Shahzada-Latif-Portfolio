0d15ece5df98882de91be9d5a4646803595da40a
Madasar <madasar54321@gmail.com>
2024-04-15 22:42:12 -0400

    Commit 2

---
+++ main.py
@@ -0,0 +1,62 @@
+from flask import Flask, render_template, redirect, url_for, request, flash
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import Integer, String, Float
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+import requests
+import smtplib
+import os
+
+
+app = Flask(__name__)
+app.secret_key = os.environ.get("secret_key")
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/blog-home")
+def blog_home():
+    return render_template('blog-home.html')
+
+@app.route("/blog-post")
+def blog_post():
+    return render_template('blog-post.html')
+
+@app.route("/case-study")
+def case_study():
+    return render_template('case-study.html')
+
+@app.route("/contact", methods=["GET","POST"])
+def contact():
+    if request.method=="POST":
+        my_email=os.environ.get("email")
+        password=os.environ.get("password")
+        message = request.form['message']
+        name = request.form['name']
+        email = request.form['email']
+        with smtplib.SMTP("smtp.gmail.com") as connection:
+            connection.starttls()
+            connection.login(user=my_email,password=password)
+            connection.sendmail(from_addr=my_email,
+                                to_addrs="madasar54321@gmail.com",
+                                msg=f"Subject: Query from Shahzada's portfolio!\n\nMessage: {message}\n\nContact information of sender:\nName: {name}\n\nEmail: {email}",
+                                )
+            flash("Email sent to Shahzada!")
+            return redirect(url_for('contact'))
+    return render_template('contact.html')
+
+@app.route("/projects")
+def projects():
+    return render_template('projects.html')
+
+@app.route("/resume")
+def resume():
+    return render_template('resume.html')
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
